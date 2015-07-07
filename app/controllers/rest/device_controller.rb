20d2083d4f35977805a7424044e627b9dae7d600
Carlos Alvarez <mike@mobileaws.com>
2015-07-06 21:20:32 -0500

    * Fixing an empty created/acknowlegded dates issue.

--- app/controllers/rest/device_controller.rb
+++ app/controllers/rest/device_controller.rb
@@ -129,9 +129,9 @@ class Rest::DeviceController < Rest::ServiceController
         deviceProperty.device_mapping = deviceMapping
         deviceProperty.property = propertySearch
         deviceProperty.dismiss_duration = property[:dismiss_duration]
-        deviceProperty.dismiss_time = Time.at(property[:dismiss_time].to_i) if !property[:dismiss_time].nil?
-        deviceProperty.created_at = Time.at(property[:created_at].to_i).utc if !property[:created_at].nil?
-        deviceProperty.updated_at = Time.at(property[:created_at].to_i).utc if !property[:created_at].nil?
+        deviceProperty.dismiss_time = Time.at(property[:dismiss_time].to_i) if !property[:dismiss_time].nil? && !property[:dismiss_time].empty?
+        deviceProperty.created_at = Time.at(property[:created_at].to_i).utc if !property[:created_at].nil? && !property[:created_at].empty?
+        deviceProperty.updated_at = Time.at(property[:created_at].to_i).utc if !property[:created_at].nil? && !property[:created_at].empty?
         deviceProperty.value = property[:value]
         deviceProperty.save!
       end
