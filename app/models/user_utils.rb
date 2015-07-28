7844eae318042f88e86a7120ce7f4e6c5cc76bce
Carlos Alvarez <mike@mobileaws.com>
2015-07-28 11:39:37 -0500

    null dates fixing in reports

--- app/models/user_utils.rb
+++ app/models/user_utils.rb
@@ -2,32 +2,24 @@
 class UserUtils
 
   def self.get_day_to_show(datetime, offset)
-    if !offset.nil?
-      datetime = datetime - (offset.to_i.minutes)
-    end
+    datetime = datetime - (offset.to_i.minutes)
     return datetime
   end
 
   def self.get_begin_of_day_offset(datetime, offset)
     datetime = datetime.beginning_of_day
-    if !offset.nil?
-      datetime = datetime - (offset.to_i.minutes)
-    end
+    datetime = datetime - (offset.to_i.minutes)
     return datetime
   end
 
   def self.get_end_of_day_offset(datetime, offset)
     datetime = datetime.end_of_day
-    if !offset.nil?
-      datetime = datetime - (offset.to_i.minutes)
-    end
+    datetime = datetime - (offset.to_i.minutes)
     return datetime
   end
 
   def self.get_date_to_store_offset(datetime, offset)
-    if !offset.nil?
-      datetime = datetime.utc + (offset.to_i.minutes)
-    end
+    datetime = datetime.utc + (offset.to_i.minutes)
     return datetime
   end
 
