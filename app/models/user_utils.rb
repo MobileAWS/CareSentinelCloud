e7208d6f7be5d96d9d655fd0f2a075939b97d496
Carlos Alvarez <mike@mobileaws.com>
2015-07-28 10:55:40 -0500

    offset null fixing

--- app/models/user_utils.rb
+++ app/models/user_utils.rb
@@ -2,24 +2,32 @@
 class UserUtils
 
   def self.get_day_to_show(datetime, offset)
-    datetime = datetime - (offset.to_i.minutes)
+    if !offset.nil?
+      datetime = datetime - (offset.to_i.minutes)
+    end
     return datetime
   end
 
   def self.get_begin_of_day_offset(datetime, offset)
     datetime = datetime.beginning_of_day
-    datetime = datetime - (offset.to_i.minutes)
+    if !offset.nil?
+      datetime = datetime - (offset.to_i.minutes)
+    end
     return datetime
   end
 
   def self.get_end_of_day_offset(datetime, offset)
     datetime = datetime.end_of_day
-    datetime = datetime - (offset.to_i.minutes)
+    if !offset.nil?
+      datetime = datetime - (offset.to_i.minutes)
+    end
     return datetime
   end
 
   def self.get_date_to_store_offset(datetime, offset)
-    datetime = datetime.utc + (offset.to_i.minutes)
+    if !offset.nil?
+      datetime = datetime.utc + (offset.to_i.minutes)
+    end
     return datetime
   end
 
