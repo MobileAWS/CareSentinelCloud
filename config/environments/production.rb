660a12caaab5a10a43ae631925d1a76e6bea9109
Carlos Alvarez <mike@mobileaws.com>
2015-07-29 16:51:48 -0500

    host fixing

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,7 +2,7 @@ Rails.application.configure do
 
   config.emailAssetHost = 'http://caresentinel-maws.herokuapp.com'
 
-  config.action_mailer.default_url_options = { :host => "caresentinel-maws.herokuapp.com"}
+  config.action_mailer.default_url_options = { :host => "caresentinel-maws-dev.herokuapp.com"}
   config.action_mailer.default_options = {from: 'CareSentinel<maws.loon.test@gmail.com>' }
 
   config.action_mailer.delivery_method = :smtp
