ed2e57e5c0d8a40956a26027276d898e12b98eca
Carlos Alvarez <mike@mobileaws.com>
2016-02-02 14:20:56 -0500

    pr corrections

---
+++ README.md
@@ -0,0 +1,5 @@
+It is necessary to create the next variables of enviroment for the correct developer of app
+
+TWILIO_ACCOUNT_SID
+TWILIO_AUTH_TOKEN
+TWILIO_NUMBER
--- app/controllers/rest/sms_controller.rb
+++ app/controllers/rest/sms_controller.rb
@@ -3,14 +3,12 @@ class Rest::SmsController < Rest::SecureController
   require 'rubygems'
   require 'twilio-ruby'
 
-  AuthValidation.public_access :sms => [:send_sms]
-
   def send_sms
     return if !checkRequiredParams(:message,:phone_numbers);
 
-      Rails.logger.info 'sending message'
       message= params[:message] if params[:message].present?
       phones= params[:phone_numbers] if params[:phone_numbers].present?
+      Rails.logger.info 'Make it so Sending SMS to '+phones.to_s+' with message '+message
 
       if phones.kind_of?(Array)
           phones.each do |phone|
@@ -24,17 +22,21 @@ class Rest::SmsController < Rest::SecureController
 
     private
     def send_message(phone_number, message)
-      @client = ::Twilio::REST::Client.new ENV['TWILIO_ACCOUNT_SID'], ENV['TWILIO_AUTH_TOKEN']
-      message = @client.account.messages.create(
-          :from =>  ENV['TWILIO_NUMBER'],
-          :to => phone_number,
-          :body => message,
-          :method => 'GET',
-          :fallback_method => 'GET',
-          :status_callback_method => 'GET',
-          :record => 'false'
-      )
-      puts message.to
+
+      begin
+        @client = ::Twilio::REST::Client.new ENV['TWILIO_ACCOUNT_SID'], ENV['TWILIO_AUTH_TOKEN']
+        message = @client.account.messages.create(
+            :from =>  ENV['TWILIO_NUMBER'],
+            :to => phone_number,
+            :body => message,
+            :method => 'GET',
+            :fallback_method => 'GET',
+            :status_callback_method => 'GET',
+            :record => 'false'
+        )
+        rescue Twilio::REST::RequestError => e
+          puts e.message
+      end
     end
 
 end
